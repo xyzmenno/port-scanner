0026dd5d5e2582d45770f8bc756e422d92428e2e
xyzmenno <83548415+xyzmenno@users.noreply.github.com>
2022-10-28 15:20:57 +0200

    fix

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import socket
-import subprocess
 import sys
 
 ip_input = input("enter ip: ")
@@ -8,9 +7,12 @@ serverip = socket.gethostbyname(ip_input)
 try:
     for port in range(1,65535):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(0.5)
+
         result = s.connect_ex((serverip, port))
         if result == 0:
             print(f"port is open {port}")
+        s.close()
 
 
 except KeyboardInterrupt:
