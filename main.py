f7eef2f6e4f130c7486b53209856fb0c1f4e44fc
xyzmenno <83548415+xyzmenno@users.noreply.github.com>
2022-10-28 15:27:41 +0200

    working

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import socket
 import sys
 
-ip_input = input("enter ip: ")
-serverip = socket.gethostbyname(ip_input)
+target_input = input("enter ip: ")
+target = socket.gethostbyname(target_input)
 
 try:
     for port in range(1,65535):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(0.5)
 
-        result = s.connect_ex((serverip, port))
+        result = s.connect_ex((target_input, port))
         if result == 0:
             print(f"port is open {port}")
         s.close()
